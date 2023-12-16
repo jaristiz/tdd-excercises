dd71c2caccdad76de767b6299db24a7528feb4c3
Jhonathan E. Aristizabal <j_aristi@hotmail.com>
2023-12-16 11:20:08 -0500

    Adicionada guia para pruebas proyecto stack

--- 1-stack/problem/stack.tests/StackTests.cs
+++ 1-stack/problem/stack.tests/StackTests.cs
@@ -3,4 +3,12 @@ namespace CustomCustomStack.tests;
 public class CustomStackTest
 {
 
+    //TODO: Implementar:
+    //1- Creacion del Stack
+    //2- Adicionar un objeto al stack
+    //2B- Obtener el ultimo objeto adicionado
+    //3- Error cuando no haya objetos en el stack
+    //4- Limitar el tamaño del stack a 3
+    //5- Implementar "Inspeccionar" o "Peek".
+
 }
